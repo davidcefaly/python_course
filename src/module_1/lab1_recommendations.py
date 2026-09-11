d6da7cf07b826d18010a32550dd55219c90aba5b
davidcefaly <dmcefaly887@access.alamancecc.edu>
2026-09-11 13:42:50 -0400

    module 1 labs

--- src/module_1/lab1_recommendations.py
+++ src/module_1/lab1_recommendations.py
@@ -1,1 +1,27 @@
-# Refer to this module's readme
+def main ():
+    difficulty = input("Difficult or Casual? ")
+    if not (difficulty == "Difficult" or difficulty == "Casual"):
+        print ("Enter a valid difficulty")
+        return
+
+    players = input("Multiplayer or Single-player? ")
+    if not (players == "Multiplayer" or players == "Single-player"):
+        print ("Enter a valid number of players")
+        return
+
+    if difficulty == "Difficult" and players == "Multiplayer":
+            recomend ("Poker")
+    elif difficulty == "Difficult" and players == "Single-player":
+        recomend ("Klondike")
+    elif difficulty == "Casual" and players == "Multiplayer":
+         recomend ("Hearts")
+    else:
+         recomend ("Clock")
+
+        
+def recomend (game):
+    print ("you might like", game)
+
+
+main()
+
